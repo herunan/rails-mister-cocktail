5ab3319a81cfd43f35b19a57caeacca95bd7d2ee
Hernan Romero <hernrome@gmail.com>
2020-01-25 17:19:23 +0000

    Completed all routes, controllers and views. Need to complete front-end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_121911) do
+ActiveRecord::Schema.define(version: 2020_01_25_154513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_01_25_121911) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list' # Open it
+ingredients_serialised = open(url).read # Pull the data as a string
+ingredients = JSON.parse(ingredients_serialised) # Parse it into a Hash
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+Cocktail.create([
+  {
+    name: "Mojito",
+    description: "Irresistibly fresh and punchy, this authentic mojito cocktail recipe is a one-way salsa dance to Cuba.",
+    image: "https://images.pexels.com/photos/1146754/pexels-photo-1146754.jpeg?auto=compress&cs=tinysrgb&dpr=2&h="
+  },
+    {
+    name: "Gin & Tonic",
+    description: "It is almost impossible to imagine, let alone find a more quintessential pairing across any spirit category than the Gin & Tonic.",
+    image: "https://images.pexels.com/photos/1170599/pexels-photo-1170599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h="
+  }
+])
+
+
