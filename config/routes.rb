8df35e7426b9d6778931ac5d8e71d7324bf8999c
Hernan Romero <hernrome@gmail.com>
2020-01-31 17:55:41 +0000

    Added reviews mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   resources :cocktails, only: %i[show new create] do
     resources :doses, only: %i[new create destroy]
+    resources :reviews, only: :create
   end
 end
